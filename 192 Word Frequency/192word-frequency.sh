80ef23921c5eb481f246a07e3d52d168ae4bf96e
Garvit <garvitbudhiraja02@gmail.com>
2024-09-05 06:24:51 +0000

    Code challenge submission for question: 192 Word Frequency

---
+++ 192 Word Frequency/192word-frequency.sh
@@ -0,0 +1,2 @@
+# Read from the file words.txt and output the word frequency list to stdout.
+awk '{ for (i=1; i<=NF; i++) { count[$i]++ } } END { for (word in count) print word, count[word] }' words.txt | sort -k2,2nr
