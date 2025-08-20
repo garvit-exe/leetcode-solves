24d4708a2964eea2176ebc0fcf05b0d2ca0a94b9
Garvit <garvitbudhiraja02@gmail.com>
2025-08-20 09:48:55 +0000

    Code challenge submission for question: 169 Majority Element

---
+++ 169 Majority Element/169majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> um;
+
+        for (int i : nums) {
+            um[i]++;
+            if (um[i] > nums.size() / 2) return i;
+        }
+
+        return -1;
+    }
+};
